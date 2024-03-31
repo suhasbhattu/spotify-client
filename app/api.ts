69adc4be339513548e9090e4c8ff3852895bf27b
Suhas Bhattu <suhasbhattu@gmail.com>
2024-03-31 14:14:02 +0530

    changes for paths

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://spotify-server-nine.vercel.app",
 });
 
 export const getAuthToken = async () => {
